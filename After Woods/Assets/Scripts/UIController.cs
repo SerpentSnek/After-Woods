095c9f3711f6d59052b72ce7bd693821ac351e61
Esther Cheng <esther588@berkeley.edu>
2024-02-25 23:14:26 -0800

    Add food display in UI Controller class

--- After Woods/Assets/Scripts/UIController.cs
+++ After Woods/Assets/Scripts/UIController.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class UIController : MonoBehaviour
 {
+    [SerializeField]
+    private UnityEngine.UI.Text foodCounter;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        this.foodCounter.text = "Food";
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        this.foodCounter.text = "x";
     }
 }
