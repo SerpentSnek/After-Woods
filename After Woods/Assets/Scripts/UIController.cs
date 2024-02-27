a703cb6d6f77c750182d5dba1605063579c0c52d
Esther Cheng <esther588@berkeley.edu>
2024-02-26 17:21:01 -0800

    Add TODO comments in UI Controller class

--- After Woods/Assets/Scripts/UIController.cs
+++ After Woods/Assets/Scripts/UIController.cs
@@ -6,20 +6,26 @@ public class UIController : MonoBehaviour
 {
     [SerializeField]
     private UnityEngine.UI.Text foodCounter;
+    [SerializeField]
+    private UnityEngine.UI.Text timer;
     [SerializeField] 
     private GameObject healthBar;
     [SerializeField] 
     private GameObject radiationBar;
 
+    // TODO add variables for food components
+
     // Start is called before the first frame update
     void Start()
     {
         this.foodCounter.text = "Food";
+        this.timer.text = "Timer";
     }
 
     // Update is called once per frame
     void Update()
     {
+        // TODO change this line to include calculation for total food
         this.foodCounter.text = "x";
     }
 }
