4ef372c8318280be153e752796ecb9efe36c5e8b
l1ghtyagam1Jin <130005599+l1ghtyagam1Jin@users.noreply.github.com>
2024-02-28 13:39:15 -0800

    Update PlayerController.cs

--- After Woods/Assets/Scripts/PlayerController.cs
+++ After Woods/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Input.Command;
+using IInput.Command;
 
 public class PlayerController : MonoBehaviour
 {
-    private ICharacterCommand fire1;
-    private ICharacterCommand jump;
-    private ICharacterCommand right;
-    private ICharacterCommand left;
+    private IInputCommand fire1;
+    private IInputCommand jump;
+    private IInputCommand right;
+    private IInputCommand left;
+    private IInputCommand climbLadder;
+    private bool isClimbing = false;
     // Start is called before the first frame update
     void Start()
     {
         this.fire1 = ScriptableObject.CreateInstance<PlayerSprint>();
+        this.climbLadder = ScriptableObject.CreateInstance<PlayerClimbLadder>();
         this.jump = ScriptableObject.CreateInstance<PlayerJump>();
         this.right = ScriptableObject.CreateInstance<PlayerMoveRight>();
         this.left = ScriptableObject.CreateInstance<PlayerMoveLeft>();
@@ -29,15 +32,42 @@ public class PlayerController : MonoBehaviour
         {
             this.left.Execute(this.gameObject);
         }
-        if (Input.GetButtonDown("Space"))
+        if (isClimbing && Input.GetKey(KeyCode.UpArrow))
+        {
+            this.climbLadder.Execute(this.gameObject);
+        }
+
+    
+    }
+    void Update(){
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             this.jump.Execute(this.gameObject);
         }
-    }
-    void Update(){
+        
         if (Input.GetButtonDown("Fire1"))
         {
             this.fire1.Execute(this.gameObject);
+        }
+            
+    }
+     void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Ladder"))
+        {
+            isClimbing = true;
+            this.GetComponent<Rigidbody2D>().gravityScale = 0;
+            this.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Ladder"))
+        {
+            isClimbing = false;
+            // Restore gravity effect after climbing
+            this.GetComponent<Rigidbody2D>().gravityScale = 1;
         }
     }
 }
