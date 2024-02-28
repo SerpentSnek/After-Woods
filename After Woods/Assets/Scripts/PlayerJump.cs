bdccdeeeae56157e1afdf5ae15f8a36e760ad409
l1ghtyagam1Jin <130005599+l1ghtyagam1Jin@users.noreply.github.com>
2024-02-28 11:48:10 -0800

    Update PlayerJump.cs

--- After Woods/Assets/Scripts/PlayerJump.cs
+++ After Woods/Assets/Scripts/PlayerJump.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-using Character.Command;
+using Input.Command;
 
-namespace Character.Command
+namespace Input.Command
 {
     public class PlayerJump : ScriptableObject, ICharacterCommand
     {
