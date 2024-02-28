a2ac786717368f4871402b75065014122cdafeab
l1ghtyagam1Jin <130005599+l1ghtyagam1Jin@users.noreply.github.com>
2024-02-28 14:35:10 -0800

    Update PlayerMoveLeft.cs

--- After Woods/Assets/Scripts/PlayerMoveLeft.cs
+++ After Woods/Assets/Scripts/PlayerMoveLeft.cs
@@ -8,7 +8,7 @@ namespace IInput.Command
 {
     public class PlayerMoveLeft : ScriptableObject, IInputCommand
     {
-        private float speed = 5.0f;
+        [SerializeField] private float speed = 5.0f;
 
         public void Execute(GameObject gameObject)
         {
