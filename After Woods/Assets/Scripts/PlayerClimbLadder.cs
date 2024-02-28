869d11465b3d9c9d0663c973bf3a00bf85f69c00
l1ghtyagam1Jin <130005599+l1ghtyagam1Jin@users.noreply.github.com>
2024-02-28 14:35:38 -0800

    Update PlayerClimbLadder.cs

--- After Woods/Assets/Scripts/PlayerClimbLadder.cs
+++ After Woods/Assets/Scripts/PlayerClimbLadder.cs
@@ -5,7 +5,7 @@ public class PlayerClimbLadder : ScriptableObject, IInputCommand
 {
     public void Execute(GameObject gameObject)
     {
-        float climbSpeed = 5f;
+        [SerializeField] float climbSpeed = 5f;
         var rigidBody = gameObject.GetComponent<Rigidbody2D>();
         rigidBody.velocity = new Vector2(0, climbSpeed);
     }
