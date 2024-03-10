ad2df70135149cae0c9654b4f0f0ee638f88f009
Esther Cheng <esther588@berkeley.edu>
2024-03-10 15:01:17 -0700

    Rename DeathController to GameOverController

--- After Woods/Assets/Scripts/GameOverController.cs
+++ After Woods/Assets/Scripts/GameOverController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class DeathController : MonoBehaviour
+public class GameOverController : MonoBehaviour
 {
     public void RestartGame()
     {
