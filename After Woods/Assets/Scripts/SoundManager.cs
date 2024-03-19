27cd989dd3d58ed38c9e2d2d85a5310532f1a398
Esther Cheng <esther588@berkeley.edu>
2024-03-18 17:04:21 -0700

    Set up SoundManager

---
+++ After Woods/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+/**
+ * Adapted from "Indroduction to AUDIO in Unity" by Brackeys:
+ * https://www.youtube.com/watch?v=6OT43pvUyfY
+ *
+ * Audio assets from: https://sirental.itch.io/elemental-dungeons
+ */
+
+public class SoundManager : MonoBehaviour
+{
+    
+}
